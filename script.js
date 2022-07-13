8c2a8139cf489c2b6c8b4fe01aa58da02d7a77f6
mabarbar <mateusz.barzenc00@gmail.com>
2022-07-13 10:27:43 +0200

    hamburger update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
+const navhamb = document.querySelector(".navhamb");
 
 const handleClick = () => {
   hamburger.classList.toggle("hamburger--active");
+  navhamb.classList.toggle("navhamb--active");
 };
 
 hamburger.addEventListener("click", handleClick);
